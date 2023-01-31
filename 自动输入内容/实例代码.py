381633f3e24c536a0ddbf019216bb6405bc40d4d
SMG7777 <102707355+SMG7777@users.noreply.github.com>
2023-01-31 09:32:38 +0800

    程序文件

---
+++ 自动输入内容/实例代码.py
@@ -0,0 +1,20 @@
+import pynput
+import time
+import easygui as eg
+
+mouse = pynput.mouse.Controller() #创建虚拟鼠标
+keyboard = pynput.keyboard.Controller() #创建虚拟键盘
+
+cs = int(eg.enterbox("请输入你要输入的次数：(不能是负数）"))
+text = eg.enterbox("请输入你要输入的内容：")
+sleep = int(eg.enterbox("请输入你要每次输入之间间隔时间："))
+
+eg.msgbox("请关闭此窗口，然后开始倒计时5秒,现在请将鼠标方式你要输入框上")
+time.sleep(5)
+for i in range(cs):
+    keyboard.type(text)
+    keyboard.press(pynput.keyboard.Key.enter)
+    keyboard.release(pynput.keyboard.Key.enter)
+    time.sleep(sleep)
+
+eg.msgbox("输入完毕，请关闭此窗口退出程序")
