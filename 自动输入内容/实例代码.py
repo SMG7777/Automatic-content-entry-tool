a70ec7f51da72ec35fda02a99e9d689582fa7a9b
SMG7777 <102707355+SMG7777@users.noreply.github.com>
2023-05-01 07:10:39 +0800

    Update 实例代码.py

--- 自动输入内容/实例代码.py
+++ 自动输入内容/实例代码.py
@@ -7,7 +7,7 @@ keyboard = pynput.keyboard.Controller() #创建虚拟键盘
 
 cs = int(eg.enterbox("请输入你要输入的次数：(不能是负数）"))
 text = eg.enterbox("请输入你要输入的内容：")
-sleep = int(eg.enterbox("请输入你要每次输入之间间隔时间："))
+sleep = float(eg.enterbox("请输入你要每次输入之间间隔时间："))
 
 eg.msgbox("请关闭此窗口，然后开始倒计时5秒,现在请将鼠标方式你要输入框上")
 time.sleep(5)
